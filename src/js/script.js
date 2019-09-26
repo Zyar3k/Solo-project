c3df7f08740ee91f72af9c767c65e9b2d9f3f35f
Arkadiusz Zygan <a.zygan@gmail.com>
2019-09-26 23:41:37 +0200

    Add personaldata-html & style

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,20 @@ document.querySelector('.open-slide').addEventListener('click', function(e) {
   toggleMenu();
 });
 
+/* email validate */
+document.querySelector('form').addEventListener('submit', function(event) {
+  var isFormValidate = true;
+
+  var emailAddressInput = event.target.querySelector('input[name="email_address"]');
+  if(emailAddressInput.value.indexOf('@') < 0) {
+    isFormValidate = false;
+    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+});
+
+
 
 /* chart */
 var ctx = document.getElementById('myChart').getContext('2d');
