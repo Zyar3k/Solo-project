4c2917d0905906c4dcb7f4af7dee756dbd006604
Arkadiusz Zygan <a.zygan@gmail.com>
2019-09-26 23:54:57 +0200

    Add modals&fonts

--- src/js/script.js
+++ src/js/script.js
@@ -25,6 +25,55 @@ document.querySelector('form').addEventListener('submit', function(event) {
   return !isFormValidate ? event.preventDefault() : true;
 });
 
+/* modals */
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+document.querySelector('nav li .icon-quit').addEventListener('click', function() {
+  openModal('#quitModal');
+});
+document.querySelector('.icon-quit').addEventListener('click', function() {
+  openModal('#quitModal');
+});
+
+document.querySelector('.icon-profile').addEventListener('click', function() {
+  openModal('#modalLogin');
+});
+
+
+document.querySelector('nav li .icon-login').addEventListener('click', function() {
+  openModal('#modalLogin');
+});
+
 
 
 /* chart */
